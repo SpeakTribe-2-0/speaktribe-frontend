1713e752ca5a9ca9f91ef189fc4821689e26d1a8
untitledDev9 <ahmedsuleiman940@gmail.com>
2025-08-27 09:24:49 +0100

    added team image, just few

--- src/pages/site/About.jsx
+++ src/pages/site/About.jsx
@@ -1,207 +1,3 @@
-
-
-// import yoruba from '../../assets/yorubaImage.png'
-// import igbo from '../../assets/igboImage.png'
-// import hausa from '../../assets/hausaImage.png'
-// import team from '../../assets/team.png'
-// import bck from '../../assets/bckAbout.png'
-
-
-// const About = () => {
-//   return (
-//     <section className=' width bg-white'>
-//       <div className='py-10 px-18 flex flex-col gap-10
-//       max-mobile:px-5
-//       '>
-//         <div className="@[480px]:px-4 @[480px]:py-3 rounded-xl overflow-hidden min-h-[218px] relative">
-
-//           <div
-//             className="absolute inset-0 bg-cover bg-center z-0 "
-//             style={{
-//               backgroundImage: `
-//             linear-gradient(0deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0) 25%),
-//             url(${bck})
-//           `,
-//             }}
-//           />
-
-//           {/* Text Content */}
-//           <div className=" z-10 flex p-4 bg-green-600">
-//             <p className="bottom-5 absolute text-white tracking-light text-[28px] font-bold leading-tight">
-//               About Speak Tribe
-//             </p>
-//           </div>
-//         </div>
-//         <div>
-//           <h1 className='text-[#263238] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5'>Our Mission</h1>
-//           <p className='text-[#181411] text-base font-normal leading-normal pb-3 pt-1 px-4 text-center w-[100%] mx-auto max-mobile:px-2'>At Speak Tribe, our mission is to bridge cultural gaps and foster understanding by providing accessible and engaging language learning resources for Nigerian
-//             languages. We aim to empower individuals to connect with their heritage, communicate effectively, and appreciate the rich linguistic diversity of Nigeria.</p>
-//         </div>
-//         <div>
-//           <h1 className='text-[#263238] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5'>Languages we Offer</h1>
-//           <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4 w-[80%] mx-auto 
-
-//           max-mobile:w-[100%] max-mobile:grid-cols-1">
-//             <div className="flex flex-1 gap-3 rounded-lg border border-[#e6dfdb] bg-white p-4 items-center">
-//               <div
-//                 className="bg-center bg-no-repeat aspect-square bg-cover rounded-lg w-10 shrink-0">
-//                 <img src={yoruba} alt="" className=' rounded-[5px]' />
-//               </div>
-//               <h2 className="text-[#181411] text-base font-bold leading-tight">Yoruba</h2>
-//             </div>
-//             <div className="flex flex-1 gap-3 rounded-lg border border-[#e6dfdb] bg-white p-4 items-center">
-//               <div
-//                 className="bg-center bg-no-repeat aspect-square bg-cover rounded-lg w-10 shrink-0">
-//                 <img src={igbo} alt="" className=' rounded-[5px]' />
-//               </div>
-//               <h2 className="text-[#181411] text-base font-bold leading-tight">Igbo</h2>
-//             </div>
-//             <div className="flex flex-1 gap-3 rounded-lg border border-[#e6dfdb] bg-white p-4 items-center">
-//               <div
-//                 className="bg-center bg-no-repeat aspect-square bg-cover rounded-lg w-10 shrink-0">
-//                 <img src={hausa} alt="" className=' rounded-[5px]' />
-//               </div>
-//               <h2 className="text-[#181411] text-base font-bold leading-tight">Hausa</h2>
-//             </div>
-//           </div>
-//           <p className=' text-center italic text-[13px]'>more soon...</p>
-//         </div>
-
-
-
-
-
-
-
-//         <div>
-//           <h1 className='text-[#263238] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5'>Meet Our Team</h1>
-//           <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4 place-items-center">
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Aisha Adebayo</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Yoruba Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Chukwudi Okoro</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Igbo Language Expert</p>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-3 text-center pb-3">
-//               <div className="px-4">
-//                 <div>
-//                   <img src={team} alt="" className="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-full" />
-//                 </div>
-//               </div>
-//               <div>
-//                 <p className="text-[#181411] text-base font-medium leading-normal">Fatima Hassan</p>
-//                 <p className="text-[#8a7260] text-sm font-normal leading-normal">Hausa Language Expert</p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div></div>
-//       </div>
-//     </section>
-//   );
-// };
-
-
-
-// export default About
-
-
-
-
-import React from 'react'
 import SideBar from '../../components/SideBar'
 import selection from '../../assets/Selection.png'
 import selection2 from '../../assets/Selection (1).png'
@@ -210,20 +6,26 @@ import { IoMicOutline } from "react-icons/io5";
 import { GiBookmarklet } from "react-icons/gi";
 import { GiProgression } from "react-icons/gi";
 import { IoPeopleOutline } from "react-icons/io5";
+
+
+
+import ahmed from '../../assets/team/ahmed.jpg'
+import muhammed from '../../assets/team/muhammed.jpg'
+import ariyo from '../../assets/team/ariyo.jpg'
 const About = () => {
   const groupMembers = [
-    { name: 'Ahmed Suleiman', position: 'Team-Leader', work: 'Full-Stack', image: team },
+    { name: 'Ahmed Suleiman', position: 'Team-Leader', work: 'Full-Stack', image: ahmed },
     { name: 'Hameedah Lawal', position: 'Ass-Team-Leader', work: 'Frontend', image: team },
     { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
+    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend / Tester', image: team },
+    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Product Manager', image: team },
     { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
-    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Frontend', image: team },
+    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Research', image: team },
+    { name: 'Muhammad Barkindo', position: 'Member', work: 'Research', image: muhammed },
+    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Research', image: team },
+    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Research', image: team },
+    { name: 'Ariyo Blessing', position: 'Member', work: 'Research', image: ariyo },
+    { name: 'Blessing Gbadamosi', position: 'Member', work: 'Research', image: team },
   ]
 
   const features1 = [
@@ -234,27 +36,27 @@ const About = () => {
   ]
   return (
     <div className=" flex gap-8 width
-    max-tablet:gap-6 max-tablet:px-3 pt-24
+    max-tablet:gap-3 max-tablet:px-3 pt-22
     ">
       <SideBar />
-      <div>
+      <div className=' '>
         <div className=' flex flex-col gap-8'>
-          <div className=' flex flex-col gap-8'>
+          <div className=' flex flex-col gap-8 max-mobile:gap-4'>
             <p className=' text-[40px] leading-11 w-[60%] mx-auto font-semibold text-center
             max-tablet:w-full max-tablet:text-[35px]
-            max-mobile:text-[30px] max-mobile:leading-8 max-mobile:my-5
+            max-mobile:text-[25px] max-mobile:leading-8 max-mobile:my-5
             '>
               SpeakTribe: Your journey to Fluency Begins
             </p>
             <p className=' w-[65%] text-[#7a7a7a] mx-auto
-            max-tablet:w-[90%]
+            max-tablet:w-[86%]
             '>
               SpeakTribe is more than just a language app; it's a vibrant community dedicated to fostering cultural connection and linguistic mastery. We believe learning a new language opens up a new worlds, and we're here to guide you every step of the way.
             </p>
           </div>
           <div className=' flex justify-center w-[1000px] mx-auto
-          max-tablet:w-[600px]
-          max-mobile:w-[400px]
+          max-tablet:w-[500px]
+          max-mobile:w-[350px]
           '>
             <img src={selection} alt="" className=' rounded-2xl' />
           </div>
@@ -330,8 +132,8 @@ const About = () => {
           max-tablet:w-[90%]
           '>SpeakTribe was born from a simple yet profound idea: that language learning should be an adventure, not a chore. Our founders, a group of passionate linguists and technologists, envisioned a platform that celebrated the journey of acquiring a new language, making it feel as natural and enriching as joining a new community.</p>
           <div className=' flex justify-center w-[1000px] mx-auto
-          max-tablet:w-[600px]
-          max-mobile:w-[400px]
+          max-tablet:w-[500px]
+          max-mobile:w-[350px]
           '>
             <img src={selection2} alt="" className=' rounded-2xl' />
           </div>
