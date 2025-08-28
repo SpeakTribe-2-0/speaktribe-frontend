e6e94f9f68abc99ff23a5295a9851be3afbdc45b
untitledDev9 <ahmedsuleiman940@gmail.com>
2025-08-28 16:44:49 +0100

    dashboard

--- src/pages/site/Dashboard.jsx
+++ src/pages/site/Dashboard.jsx
@@ -76,7 +76,7 @@ const Dashboard = () => {
     <div className='flex pt-24'>
       <SideBar />
 
-      <div className='p-6 w-full'>
+      <div className='p-6 w-full width'>
         {/* Greeting */}
         <motion.h1
           initial={{ opacity: 0, y: -20 }}
