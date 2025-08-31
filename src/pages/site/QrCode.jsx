0b2cc8c132e2ccbd86465025e8d3e4a1b3acf674
untitledDev9 <ahmedsuleiman940@gmail.com>
2025-08-31 10:18:52 +0100

    adjusted qr code page

--- src/pages/site/QrCode.jsx
+++ src/pages/site/QrCode.jsx
@@ -3,7 +3,7 @@ import qrcode from '../../assets/obQqih.png'
 
 const QrCode = () => {
   return (
-    <div className=' flex flex-col gap-5 justify-center items-center mt-24'>
+    <div className=' flex flex-col gap-5 justify-center items-center mt-24 pt-16'>
       <h1 className='text-[35px] font-semibold '>SpeakTribe</h1>
       <div className=' w-[38%] max-desktop:w-[50%]  max-tablet:w-[55%] max-mobile:w-[80%]'>
         <img src={qrcode} alt="" />
